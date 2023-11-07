215db474d648c9ae10407e302c286b97f755762b
jensbeh <jensbehmenburg@gmail.com>
2023-11-07 10:52:40 +0100

    Init

---
+++ Black Jack CodeCamp/View/ContentView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  Black Jack CodeCamp
+//
+//  Created by Jens Behmenburg on 07.11.23.
+//
+
+import SwiftUI
+import SwiftData
+
+struct ContentView: View {
+
+    var body: some View {
+        Text("Black Jack")
+    }
+}
+
+#Preview {
+    ContentView()
+}
